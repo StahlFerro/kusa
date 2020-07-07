25a521ac5a45da6e748893c3274800262899e255
StahlFerro <StahlFerro@users.noreply.github.com>
2020-07-08 03:16:04 +0700

    Fixed JPARepository long to Long

--- src/main/java/com/stahlferro/kusa/repositories/KeyCardRepository.java
+++ src/main/java/com/stahlferro/kusa/repositories/KeyCardRepository.java
@@ -3,6 +3,6 @@ package com.stahlferro.kusa.repositories;
 import com.stahlferro.kusa.models.KeyCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface KeyCardRepository extends JpaRepository<KeyCard, long>{
+public interface KeyCardRepository extends JpaRepository<KeyCard, Long>{
 
 }
